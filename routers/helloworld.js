24a0b8f523947f0c18bfb1934591345745aaee1d
BrunoPCH <bruno.perez.ch@gmail.com>
2023-10-16 16:29:23 -0600

    quite despedida de helloworld

--- routers/helloworld.js
+++ routers/helloworld.js
@@ -5,7 +5,5 @@ const router = express.Router();
 router.get("/helloworld", function (request, response) {
   response.send("Hola BEDUTEAM 🖖🏼");
 });
-router.get("/godbye", function (request, response) {
-  response.send("¡¡¡Adios mundo cruel 💁🏼‍♂️!!!");
-});
+
 module.exports = router;
