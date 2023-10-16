9177117b137f785988e3c7a5a71432dde25439bb
BrunoPCH <bruno.perez.ch@gmail.com>
2023-10-16 14:51:48 -0600

    Saludo correjido

--- routers/helloworld.js
+++ routers/helloworld.js
@@ -3,7 +3,7 @@ const { func } = require("joi");
 const router = express.Router();
 
 router.get("/helloworld", function (request, response) {
-  response.send("Hello world 🖖🏼");
+  response.send("Hola BEDUTEAM 🖖🏼");
 });
 router.get("/godbye", function (request, response) {
   response.send("Adios mundo cruel 💁🏼‍♂️!!!");
