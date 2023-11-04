74c82b320b6635a6e400885d4ddc219d63464f36
BrunoPCH <bruno.perez.ch@gmail.com>
2023-11-04 00:29:24 -0600

    DogbreedSeed, reduccion codigo. Creo no sirve rela

--- seeders/dogBreedsSeed.js
+++ seeders/dogBreedsSeed.js
@@ -1,71 +1,15 @@
-// const { Sequelize } = require("sequelize");
 const Dog = require("../models/dog");
 
 const { connect, sync, sequelize } = require("../models/sequelize");
 
-// const sequelize = new Sequelize({
-//   dialect: "sqlite",
-//   storage: "./perrinPerrout.db",
-// });
-
-// async function connect() {
-//   try {
-//     await sequelize.authenticate();
-//     console.log(">>😎 Conectado a la base de datos");
-//   } catch (e) {
-//     console.error("<<😤 No se pudo conectar a la BD ");
-//     console.error(e);
-//   }
-// }
 connect();
 
-const DogBreed = require("../models/dogbreed");
-// // llamando al model
-// const { DataTypes } = require("sequelize");
-// const DogBreed = sequelize.define("dogBreeds", {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     unique: true,
-//     autoIncrement: true,
-//   },
-//   breed: {
-//     type: DataTypes.STRING(60),
-//     unique: true,
-//     allowNull: false,
-//     primaryKey: true,
-//   },
-//   group: {
-//     type: DataTypes.STRING(60),
-//   },
-//   section: {
-//     type: DataTypes.STRING(60),
-//   },
-//   country: {
-//     type: DataTypes.STRING(60),
-//   },
-//   urlFCI: {
-//     type: DataTypes.STRING(200),
-//   },
-//   urlImage: {
-//     type: DataTypes.STRING(200),
-//   },
-//   pdfBreed: {
-//     type: DataTypes.STRING(200),
-//   },
-// });
+const DogBreed = require("../models/dogBreed");
+
+//Estableciendo relaciones
 DogBreed.hasMany(Dog);
 Dog.belongsTo(DogBreed);
 
-// async function sync() {
-//   try {
-//     await sequelize.sync({ force: false });
-//     console.log("Base de datos actualizada 🤓");
-//   } catch (e) {
-//     console.error("No se puede actualizar la BD 😭");
-//     console.error(e);
-//   }
-// }
 sync();
 
 const DogbreedData = require("../informationResources/fciBreedsData.json");
